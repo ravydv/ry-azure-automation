ede57cfcc1e3386b6f21e484eb36c27b73b813a2
Ravi <ravydv@outlook.com>
2021-10-18 11:15:46 +0530

    Delete SetupServer.ps1

--- SetupServer.ps1
+++
@@ -1,9 +0,0 @@
-Configuration SetupServer {
-    # test tags
-    Node localhost {
-                               WindowsFeature IIS {
-                               Name = "Web-Server";
-            Ensure = "Present"
-        }
-    }
-}
